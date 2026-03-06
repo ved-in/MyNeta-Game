813e33359a81ad51e30050c3295944754d9bb947
ved-in <vedantihs@gmail.com>
2026-03-06 02:37:21 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Neta Dossier
+
+Fetches MP and MLA data from the MyNeta API and loads it into the browser.
+
+Currently: bare fetch, hits CORS error in browser.
+Next: fix CORS using a proxy.
+
+I forgot to upload it to github :P
+Im a bit confused whether I like K&R style or Allman style... I like K&R in functions but it looks weird in if, for, while statements... For them Allman looks nice.. Maybe a hybrid?
---
+++ data.js
@@ -0,0 +1,76 @@
+const API_BASE = 'https://nish.space/my_neta';
+
+const MP_YEARS = ['2024', '2019', '2014', '2009', '2004'];
+
+const MLA_STATES = [
+  'maharashtra', 'andhra_pradesh', 'telangana', 'karnataka', 'kerala',
+  'tamil_nadu', 'gujarat', 'rajasthan', 'madhya_pradesh', 'uttar_pradesh',
+  'west_bengal', 'bihar', 'odisha', 'punjab', 'haryana', 'delhi', 'goa'
+];
+
+function pick_mp_fields(mp, year)
+{
+  return {
+    name: mp.name,
+    constituency: mp.constituency,
+    party: mp.party,
+    criminal_cases: mp.criminal_cases,
+    assets: mp.assets,
+    education: mp.education,
+    state: mp.state_or_ut,
+    type: 'MP',
+    year: year
+  };
+}
+
+function pick_mla_fields(mla, state_name)
+{
+  return {
+    name: mla.name,
+    constituency: mla.constituency,
+    party: mla.party,
+    criminal_cases: mla.criminal_cases,
+    assets: mla.assets,
+    education: mla.education,
+    state: state_name,
+    type: 'MLA',
+    year: 'Latest'
+  };
+}
+
+async function fetch_pool()
+{
+  const all = [];
+
+  for (let i = 0; i < MP_YEARS.length; i++) {
+    const year = MP_YEARS[i];
+    const response = await fetch(API_BASE + '/mps/' + year);
+    const data = await response.json();
+    const mps = data.mps || [];
+
+    for (let j = 0; j < mps.length; j++) {
+      all.push(pick_mp_fields(mps[j], year));
+    }
+  }
+
+  for (let i = 0; i < MLA_STATES.length; i++) {
+    const state = MLA_STATES[i];
+    const response = await fetch(API_BASE + '/mlas/' + state);
+    const data = await response.json();
+    const mlas = data.mlas || [];
+    const state_name = data.state || state.replace(/_/g, ' ');
+
+    for (let j = 0; j < mlas.length; j++) {
+      all.push(pick_mla_fields(mlas[j], state_name));
+    }
+  }
+
+  const with_crimes = [];
+  for (let i = 0; i < all.length; i++) {
+    if (all[i].criminal_cases !== null && all[i].criminal_cases !== undefined) {
+      with_crimes.push(all[i]);
+    }
+  }
+
+  return with_crimes;
+}
